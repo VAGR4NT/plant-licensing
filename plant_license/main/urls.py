71159f22e392c114bc3f62819d9f1479cdb1d53a
Wkamp <eiron.w@proton.me>
2025-10-03 21:39:42 -0400

    migrated pages to django

---
+++ plant_license/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home_view, name="home"),
+    path("direct-access/", views.direct_access_view, name="direct_access"),
+    path("view/", views.view_db_view, name="view_db"),
+    path("generate-forms/", views.generate_forms_view, name="generate_forms"),
+    path("update/", views.update_view, name="update"),
+    path("user-info/", views.user_info_view, name="user_info"),
+]
---
+++ plant_license/main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+
+def home_view(request):
+    return render(request, "main/index.html")
+
+
+def direct_access_view(request):
+    return render(request, "main/direct-access/index.html")
+
+
+def view_db_view(request):
+    return render(request, "main/view/index.html")
+
+
+def generate_forms_view(request):
+    return render(request, "main/generate-forms/index.html")
+
+
+def update_view(request):
+    return render(request, "main/update/index.html")
+
+
+def user_info_view(request):
+    return render(request, "main/user-info/index.html")
